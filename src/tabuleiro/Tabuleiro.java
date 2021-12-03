c88055a3d72dc15207cc852fb5e23581a69f30b8
Israel França <israelfranca7@gmail.com>
2021-12-03 17:22:25 -0300

    TabuleiroException e programação Defensiva

--- src/tabuleiro/Tabuleiro.java
+++ src/tabuleiro/Tabuleiro.java
@@ -1,46 +1,60 @@
 package tabuleiro;
 
 public class Tabuleiro { // Tabuleiro ou Board
-	
-	private int linha;
-	private int coluna;
+
+	private int linhas;
+	private int colunas;
 	private Piece[][] pieces;
-	
+
 	public Tabuleiro(int linha, int coluna) {
-		this.linha = linha;
-		this.coluna = coluna;
+		if(linha < 1 || coluna < 1)
+			throw new TabuleiroException("Erro criando tabuleiro: é necessário que haja pelo menos 1 linha e 1 coluna.");
+		this.linhas = linha;
+		this.colunas = coluna;
 		pieces = new Piece[linha][coluna];
 	}
 
-	
+
 	public Piece piece(int linha, int coluna) {
+		if(!positioExists(linha, coluna))
+			throw new TabuleiroException("Posicao não existe no tabuleiro");
 		return pieces[linha][coluna];
 	}
-	
+
 	public Piece piece(Posicao posicao) {
+		if(!positioExists(posicao))
+			throw new TabuleiroException("Posicao não existe no tabuleiro");
 		return pieces[posicao.getLinha()][posicao.getColuna()];
 	}
-	
+
 	public void placePiece(Piece piece, Posicao posicao) {
+		if(thereIsAPiece(posicao))
+			throw new TabuleiroException("já existe uma peça nessa posição: " + posicao);
 		pieces[posicao.getLinha()][posicao.getColuna()] = piece;
 		piece.posicao = posicao;
+	}
+
+	public boolean positioExists (int linha, int coluna) {
+		return linha >= 0 && linha < this.linhas && coluna >= 0 && coluna < this.colunas;
+	}
+
+	public boolean positioExists (Posicao posicao) {
+		return positioExists(posicao.getLinha(), posicao.getColuna());
+	}
+
+	public boolean thereIsAPiece(Posicao posicao) {
+		if(!positioExists(posicao))
+			throw new TabuleiroException("Posicao não existe no tabuleiro");
+		return piece(posicao) != null;
 	}
 	
 	// ----------------------------------------------
 	public int getLinha() {
-		return linha;
-	}
-
-	public void setLinha(int linha) {
-		this.linha = linha;
+		return linhas;
 	}
 
 	public int getColuna() {
-		return coluna;
+		return colunas;
 	}
 
-	public void setColuna(int coluna) {
-		this.coluna = coluna;
-	}
-	
 }
